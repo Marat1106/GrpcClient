d84ffe63d4721cd553751637c842482cb9b25972
Marat Mukiyat <m.mukiyat@astanait.edu.kz>
2021-01-10 20:27:27 +0600

    second project

---
+++ Grpc/Program.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Grpc
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           // Console.Write("Enter your ID:");
+            //int id = Convert.ToInt32(Console.ReadLine());
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Student.StudentClient(channel);
+            //using (var call= client.GetAllStudents(new StudentAllLoopUp()))
+            //{
+
+            //  while(await call.ResponseStream.MoveNext())
+            //{
+            //  var reply = call.ResponseStream.Current;
+            //Console.WriteLine($"{reply.User.Id}-{reply.User.Id}-{reply.User.Name}-{reply.User.Surname},{(reply.User.Gender ? "Male" : "Female")}with Gpa={reply.Gpa}");
+
+            //                }
+
+            var client = new Category.ProductClient(channel);
+            using (var call=client.GetAllProducts(new CategoryAllLookUp())) { 
+            
+            while(await call.ResponseStream.MoveNext())
+                {
+                    var reply = call.ResponseStream.Current;
+
+                    Console.WriteLine($"{reply.User.id}-{reply.User.name}--{reply.User.parentCategoryId}with Description{reply.description}--{reply.price}");
+                }
+                    }
+
+        }
+    }
+}
